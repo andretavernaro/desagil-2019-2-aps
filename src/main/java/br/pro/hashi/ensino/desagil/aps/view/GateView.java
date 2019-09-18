df04f305f7e7897a98fd57759e5894a9de7557a0
roguetaver <andretavernaro@gmail.com>
2019-09-17 21:10:27 -0300

    aps2

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -1,4 +1,78 @@
 package br.pro.hashi.ensino.desagil.aps.view;
 
-public class GateView {
+import br.pro.hashi.ensino.desagil.aps.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.Switch;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class GateView extends JPanel implements ActionListener {
+
+    private final Gate gate;
+
+    private final JCheckBox inputA;
+    private final JCheckBox inputB;
+    private final Switch SWA;
+    private final Switch SWB;
+    private final JCheckBox output;
+
+    public GateView(Gate gate) {
+        this.gate = gate;
+
+        inputA = new JCheckBox();
+        inputB = new JCheckBox();
+        SWA = new Switch();
+        SWB = new Switch();
+        output = new JCheckBox();
+
+        JLabel LinputA = new JLabel("Entrada A");
+        JLabel LinputB = new JLabel("Entrada B");
+        JLabel Loutput = new JLabel("Saída");
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        if (gate.getInputSize() == 2) {
+            add(LinputA);
+            add(inputA);
+
+            add(LinputB);
+            add(inputB);
+        } else {
+            add(LinputA);
+            add(inputA);
+        }
+        add(Loutput);
+        add(output);
+
+        inputA.addActionListener(this);
+        inputB.addActionListener(this);
+
+        output.setEnabled(false);
+
+        update();
+    }
+
+    private void update() {
+        if (inputA.isSelected()) {
+            SWA.turnOn();
+        }
+        if (inputB.isSelected()) {
+            SWB.turnOn();
+        }
+        if (gate.getInputSize() == 2) {
+            gate.connect(0, SWA);
+            gate.connect(1, SWB);
+        } else {
+            gate.connect(0, SWA);
+        }
+        output.setSelected(gate.read());
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        update();
+    }
 }
